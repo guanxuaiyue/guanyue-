bce28b96a98bf57819527b3931e89ca557cf55b6
guanxuaiyue <1099338512@qq.com>
2018-01-02 09:22:54 +0800

    1.2

--- node1.js
+++ node1.js
@@ -1,75 +1,95 @@
-var http=require('http')
-var  queryString = require('querystring');
-var fs=require('fs')
-    http.createServer(function (req,res) {
-        res.setHeader('Access-Control-Allow-Origin','*');
-        var  str=''
-      req.on('data',function (data) {
-                str+=data ;
-      })
-         req.on('end',function (err) {
-                  if (err) throw  err
-           var json=queryString.parse(str)
-        fs.readFile('./data.txt','utf8',function (err,data) {
-            if (err) throw  err
-            var json1= JSON.parse(data)
-              if (json1[json.user]){
-                            res.write('账户已注册')
-                          res.end()
-              }else {
-                  json1[json.user] = json.pass;  //json对象赋值{"xiebin":"123456"}
-                  fs.writeFile('./data.txt',JSON.stringify(json1),function(err){
-                     if (err) throw  err
-                      res.write('注册成功')
-                    res.end()
-                 })
+const express = require('express');
+const bodyParser = require('body-parser');
+const static = require('express-static');
+const fs = require('fs');
+const app = express();
+const cookieParser = require('cookie-parser');
 
-              }
+app.listen(8004,function(){
 
-        })
-
-         })
-
-    }).listen(8000)
-
-http.createServer(function(req,res){
-    res.setHeader('Access-Control-Allow-Origin','*');
-    var str = '';
-    req.on('data',function(data){
-        str += data;
-    })
-    req.on('end',function(err){
-        if(err) throw err;
-        var  json = queryString.parse(str);
-        fs.readFile('./data.txt','utf8',function(err,data){
-            if(err) throw err;
-           var  json1 = JSON.parse(data);
-            if(json1[json.user] == json.pass){
-                res.write('登陆成功');
-                res.end();
-            }else{
-                res.write('账号密码错误');
-                res.end();
-            }
-        })
-    })
-}).listen(8002)
-
-
-http.createServer(function(req,res){
-    res.setHeader('Access-Control-Allow-Origin','*');
-    var str=""
-    req.on("data",function(date){
-        str+=date
-    })
-    req.on('end',function(err){
-        if(err) throw err;
-        var json=queryString.parse(str)
-        console.log(json)
-        fs.writeFile('./wenzhang.txt',json.user+json.pass,function(err){
-            if (err) throw err;
-        })
-        res.write(json.user+json.pass)
-        res.end()
 })
-}).listen(8004)
+app.use(cookieParser("dsfuiehiknsksdifjsodfjodsjf"))
+
+app.use(bodyParser.urlencoded({}));
+
+app.use('/resign',function(req,res){
+   
+    fs.readFile('./data.txt','utf8',function(err,data){
+        if(err) throw err;
+        let date = JSON.parse(data) // object
+        let json = req.body;
+        if(date[json.user]){
+            res.send('您的账号已被注册');
+        }else{
+            //{xiebin:123456}
+            date[json.user] = json.pass;
+            fs.writeFile('./data.txt',JSON.stringify(date),function(err){
+                if(err) throw err;
+                res.send('注册成功')
+            })
+        }
+    })
+})
+
+app.use('/login',function(req,res){
+    fs.readFile('./data.txt','utf8',function(err,data){
+        if(err) throw err;
+        let date = JSON.parse(data);
+        let json = req.body;
+
+        if(date[json.user] == json.pass){
+            res.send('登陆成功')
+        }else{
+            res.send('账号密码错误')
+        }
+
+
+    })
+})
+app.use('/zidong',function(req,res){
+var json=req.body
+ req.secret="dsfuiehiknsksdifjsodfjodsjf"
+
+fs.readFile('./data.txt','utf8',function(err,data){
+    var date = JSON.parse(data)
+   
+    if (err) throw err
+        if(date[json.user] == json.pass){
+         res.cookie('user',json.user,{signed:true,maxAge:1000*60*5})
+         res.cookie('pass',json.pass,{signed:true,maxAge:1000*60*5})
+         res.send('登陆成功')
+        }
+
+
+})
+
+
+
+})
+app.use("/automatic",function(req,res){
+var neihan=req.signedCookies
+
+fs.readFile("./data.txt","utf8",function(err,data){
+if (err) throw err
+if(neihan.user==undefined){
+    res.send('密码过期')   
+}else{
+    res.send('登陆成功')
+
+}
+
+
+})
+
+
+})
+
+
+
+
+
+
+
+
+
+app.use(static('./public'))
